aaa3788771aaac562d44bb3c6055f137b699c1c2
@carina.takacs <carina.takacs@student.upt.ro>
2025-03-08 15:25:20 +0200

    am

--- Homework/homeowrk_m1.py
+++ Homework/homeowrk_m1.py
@@ -1,6 +1,9 @@
 main_dictionary={}
 while True:
+
     switch_name=input("Enter your switch name: ")
+    if switch_name=='q':
+        break
     if main_dictionary.get(switch_name) is None:
         main_dictionary[switch_name]={}
 
@@ -10,9 +13,18 @@ while True:
                 break
             if main_dictionary[switch_name].get(port_name) is None:
                 main_dictionary[switch_name][port_name]={}
+                main_dictionary[switch_name][port_name]['vlans']=[]
+                while True:
+                    vlan_nr=(input("Enter vlan number: "))
+                    if vlan_nr=='q':
+                        break
+                    else:
+                        main_dictionary[switch_name][port_name]['vlans'].append(int(vlan_nr))
+                main_dictionary[switch_name][port_name]['vlans'] = list(set(main_dictionary[switch_name][port_name]['vlans']))
             else:
                 print("Port name already exists")
             print(main_dictionary[switch_name])
+
 
 
 
