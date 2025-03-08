e5eeb8443419b15c03f5c1ba9b2bfa35840aa6c7
@carina.takacs <carina.takacs@student.upt.ro>
2025-03-08 14:53:00 +0200

    tema 1

--- Homework/homeowrk_m1.py
+++ Homework/homeowrk_m1.py
@@ -1,4 +1,23 @@
 main_dictionary={}
-switch_name=input("Enter your switch name: ")
-main_dictionary[switch_name]={}
-print(main_dictionary)
+while True:
+    switch_name=input("Enter your switch name: ")
+    if main_dictionary.get(switch_name) is None:
+        main_dictionary[switch_name]={}
+
+        while True:
+            port_name=input("Enter port name ('q' to quit): ")
+            if port_name =='q':
+                break
+            if main_dictionary[switch_name].get(port_name) is None:
+                main_dictionary[switch_name][port_name]={}
+            else:
+                print("Port name already exists")
+            print(main_dictionary[switch_name])
+
+
+
+    else:
+        print("You have already entered the switch name")
+
+
+    print(main_dictionary)
