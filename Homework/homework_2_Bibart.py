5fef23fafa9f094007f0bb27c43af38705538f05
BibartAlexandru <bibartalexandruixc@gmail.com>
2025-03-19 18:30:55 +0200

    modified homework_2 folder

--- Homework/homework_2_Bibart.py
+++ Homework/homework_2_Bibart.py
@@ -1,22 +1,23 @@
-cart = {'apple': 10, 'plums': 15, 'bananas': 5}
+# cart = {'apple': 10, 'plums': 15, 'bananas': 5}
+#
+# shop_K = {'apple': 1.2, 'plums': 4, 'bananas': 5.5}
+# shop_P = {'apple': 1.3, 'plums': 3, 'bananas': 8}
+# shop_L = {'apple': 1.4, 'plums': 2}
+#
+# shops = {'pro': shop_P, 'lil': shop_L, 'kau': shop_K}
+#
+# def best_buy(cart, shops):
+#     shop_prices = {}
+#     for shop_name, shop_dict in shops.items():
+#         if all([item in shop_dict for item in cart]):
+#             shop_prices[shop_name] = sum([
+#                 cart[item] * shop_dict[item] for item in cart
+#             ])
+#     shop_prices = list(shop_prices.items())
+#     shop_prices.sort(key=lambda x: x[1])
+#     print(shop_prices)
+#     return {
+#         "shop_name":shop_prices[0][0]
+#     }
+# print(best_buy(cart, shops))
 
-shop_K = {'apple': 1.2, 'plums': 4, 'bananas': 5.5}
-shop_P = {'apple': 1.3, 'plums': 3, 'bananas': 8}
-shop_L = {'apple': 1.4, 'plums': 2}
-
-shops = {'pro': shop_P, 'lil': shop_L, 'kau': shop_K}
-
-def best_buy(cart, shops):
-    shop_prices = {}
-    for shop_name, shop_dict in shops.items():
-        if all([item in shop_dict for item in cart]):
-            shop_prices[shop_name] = sum([
-                cart[item] * shop_dict[item] for item in cart
-            ])
-    shop_prices = list(shop_prices.items())
-    shop_prices.sort(key=lambda x: x[1])
-    print(shop_prices)
-    return {
-        "shop_name":shop_prices[0][0]
-    }
-print(best_buy(cart, shops))
